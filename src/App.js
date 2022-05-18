9265704cafdc41988bb1e8793ac91cfc424930cc
mansavy <100310641+mansavy@users.noreply.github.com>
2022-05-17 21:47:52 -0300

    Added html to project

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
 import "./App.css";
+import Radio from "./Radio.js";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div className="container">
+        <Radio />
+      </div>
+
+      <footer>
+        Coded by{" "}
+        <a
+          href="https://storied-lily-1fbc84.netlify.app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Mandy Savoie
+        </a>
+      </footer>
+    </div>
+  );
 }
 
 export default App;
--- src/Radio.js
+++ src/Radio.js
@@ -1,6 +1,41 @@
 import React from "react";
 import "./Radio.css";
+import axios from "axios";
 
 export default function Radio() {
-  return "Hello from Radio";
+  function handleResponse(response) {}
+
+  let key = "5d7f58b3856a468cbd32723f806e0b60";
+  let url = "https://api.spotify.com/v1/playlists/4SJKE524AgOKUXVBDQwNF2";
+  let headers = { Authorization: `Bearer ${key}` };
+
+  axios.get(url, { headers: headers }).then(handleResponse);
+
+  return (
+    <div className="Radio">
+      <div className="row">
+        <div className="col-sm-12">
+          <h1>New Wave Radio</h1>
+          Artist
+          <br />
+          Song title
+        </div>
+      </div>
+      <br />
+      <div className="row">
+        <div className="col-sm-12">
+          Play button
+          <br />
+          Skip button
+          <br />
+          volumn
+          <br />
+          Shuffle
+        </div>
+        <div className="col-sm-12">
+          Scroll of various artist names included in the playlist
+        </div>
+      </div>
+    </div>
+  );
 }
